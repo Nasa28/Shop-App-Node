c6503148eefd80e430adf6fcea8880c6fda9289a
Nasa28 <kaluchinasaagu@gmail.com>
2022-02-03 21:32:40 +0100

    Create AppError class

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-
+const morgan = require('morgan');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+
+app.use(morgan('dev'))
 app.use('/api/v1/products', productRoutes);
 
 app.use('/api/v1/orders', orderRoutes);
