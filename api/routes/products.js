c59733b3984eee726ae3fda8289b6da0d809352f
Nasa28 <kaluchinasaagu@gmail.com>
2022-02-04 13:06:56 +0100

    Implement route for Get all Products

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,11 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'This is a GET route',
-  });
-});
+const productsController = require('../controllers/productsController');
+const {allProducts} = productsController;
+
+router.get('/', allProducts);
 
 router.post('/', (req, res, next) => {
   res.status(201).json({
