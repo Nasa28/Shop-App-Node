fdf76297bce6a72c17cb4eb315e5db7305453b75
Nasa28 <kaluchinasaagu@gmail.com>
2022-02-09 07:28:39 +0100

    Add email template

--- utils/email.js
+++ utils/email.js
@@ -1,26 +1,46 @@
 const nodemailer = require('nodemailer');
+const { convert } = require('html-to-text');
 
-const sendEmail = async (options) => {
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.firstName;
+    this.url = url;
+    this.from = `Chinasa Kalu <${process.env.EMAIL_FROM}>`;
+  }
   // 1) create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return 1;
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
-  // 2) Define the email options
-  const mailOptions = {
-    from: 'Chinasa Kalu <nasa@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
-  // 3) Send the email
+  async send(template, subject) {
+    // render html for the email template,
+    const html = 'Bring in an html email template from file';
+    // 2) Define the email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: convert(html, {
+        wordwrap: 130,
+      }),
+    };
 
-  await transporter.sendMail(mailOptions);
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('Welcome', 'Welcome to Nasa Store');
+  }
 };
-
-module.exports = sendEmail;
