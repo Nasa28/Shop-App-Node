64fa29b9cef433d1faa1e0b9ab140b76d31301dc
Nasa28 <kaluchinasaagu@gmail.com>
2022-03-04 18:08:12 +0100

    Modify addItemToCart to update the quantity if the product already exist in the cart

--- controllers/cartsController.js
+++ controllers/cartsController.js
@@ -15,45 +15,38 @@ exports.getCartItems = asyncWrapper(async (req, res, next) => {
   });
 });
 
-exports.getCart = asyncWrapper(async (req, res, next) => {
-  const cart = await Cart.findById(req.params.id)
-    .populate('product user')
-    .select('-__v');
-
-  if (!cart) {
-    return next(new ErrorMsg('This cart does not exist', 404));
-  }
-
-  cart.__v = undefined;
-  res.status(200).json({
-    status: 'Success',
-    data: {
-      cart,
-    },
-  });
-});
-
 exports.addToCart = asyncWrapper(async (req, res, next) => {
   if (!req.body.product) req.body.product = req.product.id;
   if (!req.body.user) req.body.user = req.user.id;
+  const cartItemids = await Cart.find({ product: req.body.product });
+  if (cartItemids.length === 0) {
+    const newcart = await Cart.create(req.body);
 
-  let cart = await Cart.findOne({ user: req.user.id });
-  let product = await Product.findOne({ product: req.body.product });
+    res.status(200).json({
+      status: 'Success',
+      data: {
+        cart: newcart,
+      },
+    });
+  } else {
+    const newQuantity = (cartItemids[0].quantity += req.body.quantity);
 
-  if (!product) {
-    return next(new ErrorMsg('This cart does not exist', 404));
+    const cart = await Cart.findOneAndUpdate(
+      { product: req.body.product },
+      { quantity: newQuantity },
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+
+    res.status(200).json({
+      status: 'Success',
+      data: {
+        newQuantity: cart.quantity,
+      },
+    });
   }
-  // Prevent Users from add the same product twice
-  // const check = await Cart.findOne({ product: req.body.product });
-  // if (check) return next(new ErrorMsg('Product already added to cart', 400));
-  const newcart = await Cart.create(req.body);
-
-  res.status(200).json({
-    status: 'Success',
-    data: {
-      cart: newcart,
-    },
-  });
 });
 
 exports.updateCart = asyncWrapper(async (req, res, next) => {
--- routes/cartsRoute.js
+++ routes/cartsRoute.js
@@ -4,13 +4,12 @@ const authController = require('../controllers/authController');
 const { protectRoutes } = authController;
 const router = express.Router();
 
-const { getCartItems, getCart, addToCart, updateCart, deleteCart } =
-  cartsController;
+const { getCartItems, addToCart, updateCart, deleteCart } = cartsController;
 
 router
   .route('/')
   .get(protectRoutes, getCartItems)
   .post(protectRoutes, addToCart);
-router.route('/:id').get(getCart).patch(updateCart).delete(deleteCart);
+router.route('/:id').patch(updateCart).delete(deleteCart);
 
 module.exports = router;
