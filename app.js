5c96b5a63e2c408ebb9a71fcf1855a66495cf1f7
Nasa28 <kaluchinasaagu@gmail.com>
2022-03-30 09:06:06 +0100

    Allow cors

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ const reviewRoutes = require('./routes/reviewsRoute');
 const ErrorMsg = require('./utils/ErrorMsg');
 const globalErrorHandler = require('./controllers/errorController');
 
-app.use(cors());
+app.use(cors('*'));
 
 // Set security HTTP headers using helmet package
 app.use(helmet());
 
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
